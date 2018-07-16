3c3b6e5a82f8ee7ba1c27906b42eb17e9b088a40
czechowski <qqnamuniu@gmail.com>
2018-07-16 21:40:52 +0200

    rozpierdol

--- src/main/java/com/procountor/App.java
+++ src/main/java/com/procountor/App.java
@@ -1,5 +1,6 @@
 package com.procountor;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,10 +8,15 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         List<Invoice> invoices  = new ArrayList<>();
+        List<Product> products = new ArrayList<>();
 
-        Invoice invoice = new Invoice(1, Arrays.asList("ziemniak", "drugi ziemniak"));
-        Invoice invoice2 = new Invoice(1, Arrays.asList("ziemniak2", "drugi ziemniak2"));
+        Product product1 = new Product("ziemniak", 3, (double) 7);
+        Product product2 = new Product("ziemniak2", 5, (double) 9);
+        Invoice invoice = new Invoice(1, product1,);
+        Invoice invoice2 = new Invoice(2, Arrays.asList("ziemniak2", "drugi ziemniak2"));
 
+
+products.add(product1);
         invoices.add(invoice);
         invoices.add(invoice2);
 
--- src/main/java/com/procountor/Product.java
+++ src/main/java/com/procountor/Product.java
@@ -1,4 +1,41 @@
 package com.procountor;
 
+import java.util.List;
+
 public class Product {
+
+   private String productName;
+   private Integer amount;
+   private Double price;
+
+
+   public Product ( String productName, Integer amount, Double price){
+        this.productName = productName;
+        this.amount = amount;
+        this.price = price;
+
+
+       public Integer getAmount() {
+           return amount;
+       }
+
+
+       public String getProductName() {
+           return productName;
+       }
+
+
+       public Double getPrice() {
+           return price;
+       }
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "product name=" + productName +
+                ", amount=" + amount + ", price=" + price +
+                '}';
+    }
+
 }
